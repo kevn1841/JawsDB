6a2cfb9b71086641bce658b6b0e3b46e47edfa87
Kevin Shukla <kevn1841@gmail.com>
2016-08-01 19:35:35 -0400

    table set

---
+++ UNSOLVED/app/config/connection.js
@@ -0,0 +1,42 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+var mysql = require('mysql');
+
+// Edit this connection to make it work with JawsDB.
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "asdfghjkl8465",
+        database: "starwars"
+    },
+    jawsDB: {
+        port: 3306,
+        host: 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'c6x3ddadq4n2eee3',
+        password: "esw07zh49mzvcygp",
+        database: "starwars"
+        // TODO: Add your JawsDB connection info here
+    }
+}
+
+
+// TODO: connect your jawsDB connection witht he mysql connection.
+var connection = mysql.createConnection( source.jawsDb);
+
+
+
+// Replace the connection code here with one to connect to you JawsDB connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
